f784e96693a8384f6ad48ddc924aabc5bdf46c0b
Ernestosantos90 <ladiobenkz@gmail.com>
2026-03-28 20:54:03 -0300

    curso adicionado

---
+++ javascript/aula-06/index.js
@@ -0,0 +1,12 @@
+//Camel case regra de someças a sintax com a primeira letra minuscula e as outras palavras com maiuscula
+
+//o javascript possui tipagem dinamica: infere os tipos de dados
+//o javascript também é uma liguagem de tipagem fraca
+var nome = "Ernesto Santos"; // Str
+var idade = 35; // number
+var altura = 1.99; //number também
+var estudando = true; // boolean(verdadeiro ou falso)
+
+var curso = "frontend",
+  topico = "javascript basico";
+console.log(curso, topico);
